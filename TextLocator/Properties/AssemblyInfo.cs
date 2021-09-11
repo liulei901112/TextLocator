ba8d271ae561b9f6ec5e7c5e4f4cfb9dd146de90
刘磊 <liulei901112@163.com>
2021-09-11 14:53:48 +0800

    版本和版权修正

--- TextLocator/Properties/AssemblyInfo.cs
+++ TextLocator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("刘磊")]
 [assembly: AssemblyProduct("文本搜索定位器")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyCopyright("Copyright © 2021 Liulei All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,8 +49,8 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
 
 // log4net
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
